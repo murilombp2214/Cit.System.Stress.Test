23d59e9e0be05cf375668e4e41bb147b73a90a71
mbpeixoto <murilombp2214@gmail.com>
2024-03-24 17:01:30 -0300

    add load balance as hml

--- Cit.System.Rifa/RobertRealiza/Config/Enviroment.js
+++ Cit.System.Rifa/RobertRealiza/Config/Enviroment.js
@@ -1,8 +1,12 @@
-import { IsLocal } from '../../../Utils/Enviroment.js';
+import { IsHML, IsLocal } from '../../../Utils/Enviroment.js';
 
 export function getBaseUrl(){
     if(IsLocal())
         return 'http://localhost/monster_rifa/public/';
     
+    //load balance
+    if(IsHML())
+        return 'http://monster-dev-103800893.us-east-1.elb.amazonaws.com';
+
     return 'https://roberthrealiza.com/'
 }
